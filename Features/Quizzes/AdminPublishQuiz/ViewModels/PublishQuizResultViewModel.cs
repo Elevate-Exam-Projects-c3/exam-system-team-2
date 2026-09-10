f85539314f34ffd961b44c01971f8528f4d50fdc
Mohamed3-Saeed <mohamed.said.33480@gmail.com>
2026-09-10 17:50:39 +0300

    EXAM-126  Publish Endpoint & Guard

---
+++ Features/Quizzes/AdminPublishQuiz/ViewModels/PublishQuizResultViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace exam_system.Features.Quizzes.AdminPublishQuiz.ViewModels
+{
+    public class PublishQuizResultViewModel
+    {
+        public bool IsReadyToPublish { get; set; }
+        public List<PublishQuizCheckItemViewModel> Checks { get; set; } = new();
+    }
+}
