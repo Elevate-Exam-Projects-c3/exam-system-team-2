b097463e7b8baee4d089af7e838a81e6629c470a
Mohamed3-Saeed <mohamed.said.33480@gmail.com>
2026-09-14 23:55:05 +0300

    EXAM-136  Attempt Results Endpoint

---
+++ Features/Attempts/GetAttemptResults/ViewModels/QuestionResultViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace exam_system.Features.Attempts.GetAttemptResults.ViewModels
+{
+    public class QuestionResultViewModel
+    {
+        public string QuestionText { get; set; } = string.Empty;
+        public string? Explanation { get; set; }
+        public string? SelectedOptionText { get; set; }
+        public bool IsCorrect { get; set; }
+        public string? CorrectOptionText { get; set; }
+    }
+}
