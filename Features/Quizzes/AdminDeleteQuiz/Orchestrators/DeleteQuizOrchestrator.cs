10ec0dfd116062077b21947ab5be99f2e91ef75d
AbdelRahman_Yassin <abdelrahman.ahmed.yassin03@gmail.com>
2026-09-14 08:09:44 +0300

    EXAM-18-Delete-Quiz-Admin is done and it will get reivew by admin

---
+++ Features/Quizzes/AdminDeleteQuiz/Orchestrators/DeleteQuizOrchestrator.cs
@@ -0,0 +1,32 @@
+﻿using exam_system.Domain.Entities.Quizzes;
+using exam_system.Features.Quizzes.AdminDeleteQuiz.Commands;
+using exam_system.Features.Shared;
+using exam_system.Persistence.DataAccess;
+using MediatR;
+using Microsoft.EntityFrameworkCore.SqlServer.Query.Internal;
+
+namespace exam_system.Features.Quizzes.AdminDeleteQuiz.Orchestrators
+{
+    public class DeleteQuizOrchestratorHandler : IRequestHandler<DeleteQuizOrchestrator, ApiResponse<bool>>
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IGenericRepository<Quiz> quizRepository;
+
+        public DeleteQuizOrchestratorHandler(IUnitOfWork unitOfWork, IGenericRepository<Quiz> quizRepository)
+        {
+            this.unitOfWork = unitOfWork;
+            this.quizRepository = quizRepository;
+        }
+
+        public async Task<ApiResponse<bool>> Handle(DeleteQuizOrchestrator request, CancellationToken cancellationToken)
+        {
+            var GetQuiz = await quizRepository.GetByIdAsync(request.QuizId);
+            if(GetQuiz is null)
+            {
+                return ApiResponse<bool>.Fail("The Exam is not found", 404);
+            }
+
+            GetQuiz.UnPublichArchived();
+        }
+    }
+}
